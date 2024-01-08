04e0cd51b672f2a01ecc4ecfdf60835eedabf671
Ricardo de Aagostini <ricardoagneto@gmail.com>
2024-01-08 15:39:48 -0300

    first commit

---
+++ Pickers Tutorial SwiftfulThinking/Pickers Tutorial SwiftfulThinking/Pickers_Tutorial_SwiftfulThinkingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pickers_Tutorial_SwiftfulThinkingApp.swift
+//  Pickers Tutorial SwiftfulThinking
+//
+//  Created by Ricardo de Agostini Neto on 08/01/24.
+//
+
+import SwiftUI
+
+@main
+struct Pickers_Tutorial_SwiftfulThinkingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
