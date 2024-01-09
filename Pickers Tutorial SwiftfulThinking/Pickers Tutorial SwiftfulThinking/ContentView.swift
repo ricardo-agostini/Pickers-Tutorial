a7710b962e6b1d6b2f52d9c91faf5125fa09202e
Ricardo de Aagostini <ricardoagneto@gmail.com>
2024-01-09 15:44:32 -0300

    finishing the tutorial

--- Pickers Tutorial SwiftfulThinking/Pickers Tutorial SwiftfulThinking/ContentView.swift
+++ Pickers Tutorial SwiftfulThinking/Pickers Tutorial SwiftfulThinking/ContentView.swift
@@ -8,14 +8,69 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var selection: String = "Most Recent"
+    let filterOptions: [String] = ["Most Recent", "Most Popular", "Most Liked"]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        
+        Picker(
+            selection: $selection,
+            label: 
+                HStack {
+                    Text("Filter:")
+                    Text(selection)
+            }
+                .font(.headline)
+                .foregroundColor(.white)
+                .padding()
+                .padding(.horizontal)
+                .background(Color.blue)
+                .cornerRadius(10)
+                .shadow(color: Color.blue.opacity(0.3),
+                        radius: 10, x: 0, y: 10)
+            ,
+            content: {
+                ForEach(filterOptions, id: \.self) { option in
+                    
+                    HStack {
+                        Text(option)
+                        Image(systemName: "heart.fill")
+                    }
+                    .tag(option)
+                    
+                }
+                
+            })
+        .pickerStyle(MenuPickerStyle())
+        
+        
+        
+//        VStack {
+//            
+//            HStack {
+//                Text("Age:")
+//                Text(selection)
+//            }
+//            
+//            
+//            Picker(
+//                selection: $selection,
+//                label: Text("Picker"),
+//                content: {
+//                    ForEach(18..<100) { number in
+//                        Text("\(number)")
+//                            .font(.headline)
+//                            .foregroundColor(.red)
+//                            .tag("\(number)")
+//                    }
+//                }
+//            )
+//            .pickerStyle(WheelPickerStyle())
+//            //.background(Color.gray.opacity(0.3))
+//            
+//        }
+
     }
 }
 
